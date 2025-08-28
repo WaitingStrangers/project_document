36534ed38387709c27de268f3ff178cf43ea3482
早晨海风 <cn.cheft@gmail.com>
2025-08-28 16:22:39 +0800

    接口访问处理

--- VueWeb/src/service/api.js
+++ VueWeb/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",   // 根据你的后端接口前缀
+    baseURL: "http://192.168.31.113:3000/api",   // 根据你的后端接口前缀
     timeout: 5000
 });
 
